21e933f60197d097cc745a3418945a88f9d223b0
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2024-10-23 11:53:47 +0200

    fix: fix variable name based on https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/ns-sysinfoapi-memorystatusex

--- rqd/rqd/rqmachine.py
+++ rqd/rqd/rqmachine.py
@@ -770,7 +770,7 @@ class Machine(object):
                             ("ullAvailPageFile", ctypes.c_ulonglong),
                             ("ullTotalVirtual", ctypes.c_ulonglong),
                             ("ullAvailVirtual", ctypes.c_ulonglong),
-                            ("sullAvailExtendedVirtual", ctypes.c_ulonglong),]
+                            ("ullAvailExtendedVirtual", ctypes.c_ulonglong),]
 
                 def __init__(self):
                     # have to initialize this to the size of MEMORYSTATUSEX
